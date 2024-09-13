93d102433ba0870f1df94111898aa4d6bd40ee83
Rodigo G Martins <mrgobetti0@gmail.com>
2024-09-13 15:27:32 -0300

    Feat: criação de teste django

--- mysite/tests/polls/test_models.py
+++ mysite/tests/polls/test_models.py
@@ -1,1 +1,16 @@
 import pytest
+from django.utils import timezone
+
+from polls.models import Question
+
+
+@pytest.mark.django_db
+def test_question_was_published_recently():
+    #GIVEN:
+    question_text = "Teste string?"
+    pub_date = timezone.now() - timezone.timedelta(days=1)
+    active = True
+    #WHEN:
+    question = Question.objects.create(question_text=question_text, pub_date=pub_date, active=active)
+    #THEN:
+    assert question.was_published_recently() is True
