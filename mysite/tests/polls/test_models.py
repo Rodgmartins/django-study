37d32d8f93e9007ec6cb73f4b1e4adfc1f4b2efb
Rodigo G Martins <mrgobetti0@gmail.com>
2024-09-13 16:13:03 -0300

    Refac: corrigindo bug do test

--- mysite/tests/polls/test_models.py
+++ mysite/tests/polls/test_models.py
@@ -1,16 +1,15 @@
 import pytest
 from django.utils import timezone
-
 from polls.models import Question
 
 
 @pytest.mark.django_db
 def test_question_was_published_recently():
     #GIVEN:
-    question_text = "Teste string?"
+    question_text = "Teste_string?"
     pub_date = timezone.now() - timezone.timedelta(days=1)
     active = True
-    #WHEN:
-    question = Question.objects.create(question_text=question_text, pub_date=pub_date, active=active)
     #THEN:
+    question = Question.objects.create(question_text=question_text, pub_date=pub_date, active=active)
+    #WHEN:
     assert question.was_published_recently() is True
